e76434a0c43194cb8dd862070a79ab77ae020ce2
cesar.lara@blueoceantech.com.mx <cesar.lara@blueoceantech.com.mx>
2019-07-08 21:31:50 -0500

    Crear y listar un servicio.
    Crear y listar un servicio.

--- app/resolvers/Mutation.js
+++ app/resolvers/Mutation.js
@@ -1,5 +1,6 @@
 const AlojamientoModel = require('../models/Alojamiento');
 const TipoAlojamientoModel = require('../models/TipoAlojamiento');
+const ServicioModel = require('../models/Servicio');
 
 /**
  * Función para crear un alojamiento.
@@ -33,9 +34,27 @@ const createTipoAlojamiento = async (root, params, context, info) => {
 	if (!TipoAlojamiento) throw new Error('No se creo el Alojamiento');
 
 	return TipoAlojamiento.toObject();
-}; 
+};
+
+/**
+ * Función para crear un servicio.
+ * @param {*} root 
+ * @param {*} params 
+ * @param {*} context 
+ * @param {*} info 
+ */
+const createServicio = async (root, params, context, info) => {
+
+	const Servicio = await ServicioModel.create(params.data)
+		.catch(e => { throw new Error(e.message); });
+
+	if (!Servicio) throw new Error('No se creo el Alojamiento');
+
+	return Servicio.toObject();
+}
 
 module.exports = {
 	createAlojamiento,
-	createTipoAlojamiento
+	createTipoAlojamiento,
+	createServicio
 };
